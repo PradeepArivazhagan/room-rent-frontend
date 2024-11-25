f1233d0f40b0f527e229034cdd0cbc0ce4a39d0c
pradeeparivazhagan <pradeeparivazhagan123@gmail.com>
2024-11-25 20:48:34 +0530

    fix: add 404 page

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,17 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./Pages/Login";
 import RentalDashboard from "./Pages/RentalDashboard";
 import ProtectedRoute from "./ProtectedRoute/ProtectedRoute";
+import NotFound from "./Pages/NotFound";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/rentalDashboard" element={<ProtectedRoute />}>
+        <Route element={<ProtectedRoute />}>
           <Route path="/rentalDashboard" element={<RentalDashboard />} />
         </Route>
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/Pages/NotFound.jsx
@@ -0,0 +1,9 @@
+const NotFound = () => {
+  return (
+    <div className="h-screen bg-blue-950 flex flex-col items-center justify-center text-white text-3xl md:text-4xl lg:text-5xl font-[Gabarito]">
+      Page Not Found
+    </div>
+  );
+};
+
+export default NotFound;
